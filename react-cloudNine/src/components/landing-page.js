7055506c1e82a118ac25b0a81357aa6744bc36d2
Adam Hinkel <ahinkel421@gmail.com>
2017-11-28 12:48:36 -0500

    adding react

---
+++ react-cloudNine/src/components/landing-page.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import './landing-page.css';
+// import Header from './header';
+
+export default class LandingPage extends React.Component {
+
+    render() {
+        return (
+            <div>
+                <nav className="navbar">
+        <h1 className="navbar-header">CloudNine</h1>
+    </nav>
+
+    {/*Page One*/}
+    <section className="page-one">
+        {/*Greeting section*/}
+        <section className="greeting-section landing-page-greeting-section">
+            <h2 className="page-header">Positive mind.</h2><h2 className="page-header">Positive life.</h2>
+        </section>
+        {/*About section*/}
+        <section className="about">
+            <h2 className="about-header">The CloudNine Experience</h2>
+            <p className="about-description">CloudNine is a safe space where people can come together and share positive experiences, inspirational quotes, or anything that will boost the morale of others. If you’re visiting CloudNine and don’t feel like sharing, that’s ok too. Step into one of our lounges below and see what others have to say. Thanks for stopping by, enjoy your stay.</p>
+        </section>
+        {/*Lounges section*/}
+        <section className="lounges">
+            <h2 className="lounges-header">Lounges</h2>
+            <div className="lounge-box rak-box">
+                <div className="rak-pic lounge-pic">
+                    <h3 className="lounge-pic-header">Random Acts of Kindness</h3>
+                </div>
+                <p className="lounge-description">Share a R.A.K. that you performed or that someone performed for you</p>
+            </div>
+
+            <div className="lounge-box pa-box">
+                <div className="pa-pic lounge-pic">
+                    <h3 className="lounge-pic-header">Personal Achievements</h3>
+                </div>
+                <p className="lounge-description">Share a personal goal that you recently achieved </p>
+            </div>
+
+            <div className="lounge-box iq-box">
+                <div className="iq-pic lounge-pic">
+                    <h3 className="lounge-pic-header">Inspirational Quotes</h3>
+                </div>
+                <p className="lounge-description">Share your favorite inspirational quote(s)</p>
+            </div>
+        </section>
+    </section>
+    
+    <footer className="footer">
+        <span className="credit">Created by Adam Hinkel</span>
+    </footer>
+            </div>
+        );
+    }
+}
+
---
+++ react-cloudNine/src/index.js
@@ -0,0 +1,14 @@
+import './reset.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './reset.css';
+import './index.css';
+
+import LandingPage from './components/landing-page';
+import Lounge from './components/lounge';
+
+ReactDOM.render(
+    <Lounge />,
+    document.getElementById('root')
+);
