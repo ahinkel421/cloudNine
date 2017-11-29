b8cd2a8f2296f889d29770955f6d99eeca8b4ae1
Adam Hinkel <ahinkel421@gmail.com>
2017-11-29 11:46:40 -0500

    switched from class to function component

--- react-cloudNine/src/components/footer.js
+++ react-cloudNine/src/components/footer.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import './footer.css';
 
-export default class Footer extends React.Component {
-	render() {
-        return (
-            <div>
-                <footer className="footer">
-                    <span className="credit">Created by Adam Hinkel</span>
-                </footer>
-            </div>
-        );
-    }
+export default function Footer() {
+    return (
+        <div>
+            <footer className="footer">
+                <span className="credit">Created by Adam Hinkel</span>
+            </footer>
+        </div>
+    );
 }
--- react-cloudNine/src/components/navbar.js
+++ react-cloudNine/src/components/navbar.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import './navbar.css';
 
-export default class Navbar extends React.Component {
-	render() {
-        return (
-            <div>
-                <nav className="navbar">
-                    <h1 className="navbar-header">CloudNine</h1>
-                </nav>
-            </div>
-        );
-    }
+export default function Navbar() {
+    return (
+        <div>
+            <nav className="navbar">
+                <h1 className="navbar-header">CloudNine</h1>
+            </nav>
+        </div>
+    );
 }
